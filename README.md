5e60e62e929d035b69407614fb1ffd3fc14a256a
James West <james.west@westdarley.co.uk>
2018-12-05 10:25:14 +0000

    second commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# IonicNativeFileChooser
+An example of using the native input type="file" chooser
+
+This should work in all thee main scenarios...
+1: Android
+2: iOS
+3: PWA
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,7 +7,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('filechooser') fileChooser: ElementRef;
+
   constructor(public navCtrl: NavController) {
+
+  }
+
+  items: File[] = [];
+
+  ionViewDidLoad() {
+
+    const wireUpFileChooser = () => {
+      const elementRef = this.fileChooser.nativeElement as HTMLInputElement;
+
+      elementRef.addEventListener('change', (evt: any) => {
+        const files = evt.target.files as File[];
+
+        for (let i = 0; i < files.length; i++) {
+          this.items.push(files[i]);
+        }
+
+      }, false);
+
+    }
+
+    wireUpFileChooser();
 
   }
 
